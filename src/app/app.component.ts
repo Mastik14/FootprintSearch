92e0e9965345b32777a297db3232bd60c1a62caa
mastik14 <www.sergeypygach@mail.ru>
2025-08-07 19:20:29 +0300

    first commit, added basic logic task

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from "@angular/core";
+import { FootprintService } from "../services/footprint.service";
+import type { Country, CountryEmissionsForYear } from "../typings/Country";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  countries: Country[] = [];
+  emissionsMap = new Map<string, CountryEmissionsForYear[]>();
+  visibleData: { country: string; carbon: number }[] = [];
+
+  currentYear = 1970;
+  maxYear = 2020;
+  intervalId: any;
+
+  constructor(private footprintService: FootprintService) {}
+
+  ngOnInit() {
+    this.footprintService.getCountries().subscribe(countries => {
+      this.countries = countries.slice(0, 10);
+
+      let loaded = 0;
+
+      this.countries.forEach(({ countryCode, shortName }) => {
+        this.footprintService.getCountry(countryCode).subscribe(data => {
+          this.emissionsMap.set(shortName, data);
+
+          data.forEach(d => {
+            if (d.year > this.maxYear) {
+              this.maxYear = d.year;
+            }
+          });
+
+          loaded++;
+          if (loaded === this.countries.length) {
+            this.startYearInterval();
+          }
+        });
+      });
+    });
+  }
+
+  startYearInterval() {
+    this.updateVisibleData();
+
+    this.intervalId = setInterval(() => {
+      this.currentYear++;
+      if (this.currentYear > this.maxYear) {
+        clearInterval(this.intervalId);
+        return;
+      }
+
+      this.updateVisibleData();
+    }, 1000);
+  }
+
+  updateVisibleData() {
+    this.visibleData = [];
+
+    this.emissionsMap.forEach((data, countryName) => {
+      const yearData = data.find(d => d.year === this.currentYear);
+      if (yearData) {
+        this.visibleData.push({
+          country: countryName,
+          carbon: yearData.carbon,
+        });
+      }
+    });
+  }
+}
